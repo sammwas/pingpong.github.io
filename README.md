354878354e9f1723923a8b779a5facf625badd8f
sammwas <samson.m.njuguna@gmail.com>
2017-06-14 13:07:44 +0300

    first commit on the project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ping-Pong Game
+
+## Author:Samson Mwangi
+
+## Project Definition
+This weeks project was about creating a small game-like program called "Ping Pong.The
+basic concept is drawn from elementary  arithmetic where we use numbers and the operator
+modulus.Apart from that we also get to practice on the concepts learnt during the week such
+as
+- looping
+- arrays
+- array methods
+- array mapping
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//business logic
+var numArray=[];
+var pingpong=function(num){
+   for(var x=1;x<=num x++){
+     if(x % 15===0){
+        numArray.push("pingpong");
+     }else if (x % 5===0) {
+        numArray.push("pong");
+     }
+     else if (x % 3===0) {
+        numArray.push("ping");
+     } else {
+        numArray.push(x);
+     }
+   };
+
+};
+
+//user interface logic
+$(document).ready(function(){
+   $("form#fame").submit(function(event){
+      event.preventDefault();
+      var num=parseInt($("input#slow").val());
+      $("#swim").text("");
+      pingpong(num);
+      numArray.forEach(function(num){
+        $("#swim").append('<li>' + num + '<li>');
+
+      });
+
+  });
+});
